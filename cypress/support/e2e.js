7b6a4e6a363586da45e9df8d75cc829b26b985a1
Ivan Pyslar <i.pyslar@xtoniq.com>
2024-02-25 01:00:55 +0100

    Add missing file for cypress

---
+++ cypress/support/e2e.js
@@ -0,0 +1,13 @@
+// cypress/support/e2e.js
+
+// Import commands.js using ES2015 syntax:
+import './commands';
+
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
+
+// You can also add any custom configuration or hooks here
+// For example:
+// before(() => {
+//     // runs once before all tests in the block
+// })
